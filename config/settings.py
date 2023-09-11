5823e5fef1a111c7e4f5c8f12d31189fd33cbd7c
Mohammadreza Pourhoseini <mohammadreza.pour.computer@gmail.com>
2023-09-11 16:33:54 +0330

    Extending the existing User model

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "blog.apps.BlogConfig",
+    "apps.blog.apps.BlogConfig",
+    "apps.account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [
