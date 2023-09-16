988b6dc07b3d9e67bbbecd5c3ddfcb22d9b00115
Mohammadreza Pourhoseini <mohammadreza.pour.computer@gmail.com>
2023-09-16 17:50:43 +0330

    Create home page

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.blog.views import all_posts, post_detail
+from apps.blog.views import all_posts, post_detail, home
 
 urlpatterns = [
-    path('', all_posts, name='all_post'),
+    path('', home, name='home'),
+    path('posts/', all_posts, name='all_post'),
     path('posts/<slug:slug>/', post_detail, name='post_detail'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,8 +1,25 @@
+from django.db.models import Count
 from django.shortcuts import render
-from .models import Post, Comment
+from django.contrib.auth.models import User
+from .models import Post, Comment, Category
 
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.filter(status='Publish')[:3]
+    authors = User.objects.all()[:5]
+    categories = Category.objects.filter(status=True).annotate(num_posts=Count('post')).order_by('-num_posts')[:10]
+
+    context = {
+        'posts': posts,
+        'authors': authors,
+        'categories': categories
+
+    }
+
+    return render(request, 'blog/home.html', context)
+
+
 def all_posts(request):
     posts = Post.objects.filter(status='Publish')
 
